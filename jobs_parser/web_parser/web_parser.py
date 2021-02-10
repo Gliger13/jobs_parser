cd9ed5ea0f12e741260c19a2006051b5c0501c9b
Andrei Zaneuski <zaneuskiandrey@gmail.com>
2021-02-10 23:38:54 +0300

    Refactor: change filename and separate path gen
    Change a filename of page to save;
    Move path generation from function to the property;

--- jobs_parser/web_parser/web_parser.py
+++ jobs_parser/web_parser/web_parser.py
@@ -12,20 +12,29 @@ class WebParser:
         self.regex_templates = [re.compile(template, re.IGNORECASE) for template in regex_templates]
         self.request_headers = request_headers
 
+    @property
+    def path_to_save(self):
+        dir_name = 'downloaded_pages'
+        if os.path.split(os.path.abspath(os.path.curdir))[1] != "jobs_parser":
+            source_dir_path = os.path.split(os.path.abspath(os.path.curdir))[0]
+            return os.path.join(source_dir_path, dir_name)
+        else:
+            source_dir_path = os.path.abspath(os.path.curdir)
+            return os.path.join(source_dir_path, dir_name)
+
     def _get_page(self, url):
         page = requests.get(url, headers=self.request_headers)
         page.raise_for_status()
         return page
 
-    @staticmethod
-    def _save_page(url, url_number, page):
-        dir_name = 'downloaded_pages'
-        if not os.path.exists(dir_name):
-            os.mkdir(dir_name)
+    def _save_page(self, url, page):
+        if not os.path.exists(self.path_to_save):
+            os.mkdir(self.path_to_save)
 
-        filename = f"{hashlib.md5(url.encode('utf-8')).hexdigest()[:8]}_{url_number}.html"
-        path_to_save = os.path.join(dir_name, filename)
-        with open(path_to_save, 'w') as file:
+        filename = f"{hashlib.md5(url.encode('utf-8')).hexdigest()}.html"
+        file_path = os.path.join(self.path_to_save, filename)
+
+        with open(file_path, 'w') as file:
             file.write(page.text)
 
     @staticmethod
