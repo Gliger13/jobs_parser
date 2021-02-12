6f889e4ca994161ea2ebaf7d1f51be4a93a96471
Gliger13 <zaneuskiandrey@gmail.com>
2021-02-12 22:08:54 +0300

    Refactor: split entities from WebParser class
     - divide parse function into _parse_file and parse
     - create ParseResul and ParseResults class in parse_results.py
     - rename argument of WebParser.__init__ function

---
+++ jobs_parser/business/parse_results.py
@@ -0,0 +1,41 @@
+class ParseResult:
+    def __init__(self, url: str, words_to_find, found_words: list):
+        self.url = url
+        self.words_to_find = words_to_find
+        self.found_words = found_words
+
+    def __str__(self):
+        return f"On {self.url}: " + self.count_words_str()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def count_word(self, word):
+        return (list(map(str.title, self.found_words))).count(word.title())
+
+    def count_word_str(self, word):
+        return f"{word} - {self.count_word(word)}"
+
+    def count_words(self):
+        return {word: self.count_word(word) for word in self.words_to_find}
+
+    def count_words_str(self):
+        return ", ".join([self.count_word_str(word) for word in self.words_to_find])
+
+
+class ParseResults:
+    def __init__(self, words_to_find, parse_result):
+        self.words_to_find = words_to_find
+        self.parse_results = parse_result
+
+    def count_word_occurrence(self, word):
+        return sum([parse_result.count_word(word) for parse_result in self.parse_results])
+
+    def count_words_occurrence(self):
+        return {word: self.count_word_occurrence(word) for word in self.words_to_find}
+
+    def average_num_of_word_occur(self, word):
+        return self.count_word_occurrence(word) / len(self.parse_results)
+
+    def average_num_of_words_occur(self):
+        return {word: self.average_num_of_word_occur(word) for word in self.words_to_find}
--- jobs_parser/business/web_parser.py
+++ jobs_parser/business/web_parser.py
@@ -2,31 +2,33 @@ import re
 
 from bs4 import BeautifulSoup
 
+from business.parse_results import ParseResult, ParseResults
 from core.page_manager import Pages
 
 
 class WebParser:
-    def __init__(self, regex_templates, urls, request_headers=None):
+    def __init__(self, words_to_find, urls, request_headers=None):
         self.urls = urls
-        self.regex_templates = [re.compile(template, re.IGNORECASE) for template in regex_templates]
+        self.words_to_find = words_to_find
+        self.regex_templates = [re.compile(template, re.IGNORECASE) for template in self.words_to_find]
         self.request_headers = request_headers
 
     @staticmethod
     def _user_content(page_file):
-        return BeautifulSoup(open(page_file().file_path), 'html.parser').body.get_text()
+        return BeautifulSoup(open(page_file.file_path), 'html.parser').body.get_text()
 
-    def _parse_page(self, user_content: str):
+    def _findall_in_page(self, user_content: str):
         matched = []
         for template in self.regex_templates:
             matched.extend(re.findall(template, user_content))
         return matched
 
+    def _parse_file(self, page_file):
+        user_content = self._user_content(page_file)
+        matched = self._findall_in_page(user_content)
+        return ParseResult(page_file.url, self.words_to_find, matched)
+
     def parse(self):
         pages = Pages(self.urls, self.request_headers)
         page_files = pages.get_files()
-
-        parse_results = []
-        for page_file in page_files:
-            user_content = self._user_content(page_file)
-            parse_results.append(self._parse_page(user_content))
-        return parse_results
+        return ParseResults(self.words_to_find, [self._parse_file(page_file) for page_file in page_files])
