09be359de83f00e4cf4304d32b59704eeaae7dcb
Gliger13 <zanevskiyandrey@gmail.com>
2021-02-14 02:23:00 +0300

    Tests: add tests for web parser

--- jobs_parser/tests/test_business/test_web_parser.py
+++ jobs_parser/tests/test_business/test_web_parser.py
@@ -4,41 +4,32 @@ from business.web_parser import WebParser
 
 
 @pytest.fixture
-def request_headers():
-    return {'user-agent': 'job_parser/0.1.0'}
-
-
-@pytest.fixture(params=[
-    ('python Pythonas mvfkvm fjf python', ['python', 'Python', 'python']),
-    ('flaskFLASKfLask flask FLAK', ['flask', 'FLASK', 'fLask', 'flask']),
-    ('linux, Linux, linux lin', ['linux', 'Linux', 'linux'])
-])
-def text_to_find(request):
-    return request.param
-
-
-def (request):
-    return request.param
-
-
-@pytest.fixture
-def web_parser(request_headers):
-    words = [r'rabota']
-    return WebParser(
-        words,
-        ['https://rabota.by/feedback'],
-        request_headers=request_headers
-    )
-
-
-def text_to_find(request):
-    return request.param
+def additional_parser_params():
+    return {
+        'request_headers': {'user-agent': 'job_parser/0.1.0'},
+        'classes_to_exclude': ['recommended-vacancies', 'related-vacancies-wrapper']
+    }
 
 
 class TestWebParser:
-    def test__findall_in_page(self, web_parser, text_to_find):
-        text, result = text_to_find
-        assert web_parser._findall_in_page(text) == result
+    test_data = [
+        ((['https://hh.ru/vacancy/40447045'], ['python', 'linux']), {'python': 3, 'linux': 0}),
+        ((['https://rabota.by/vacancy/42154105'], ['Python', 'linux', 'flask']),
+         {'Python': 7, 'linux': 0, 'flask': 1}),
+        ((['https://rabota.by/vacancy/41595848'], ['Linux']), {'Linux': 2}),
+    ]
 
-    # def test_parse(self, web_parser):
-    #     assert web_parser.parse().count_word_occurrence(self.words.pop()) == 18
+    @pytest.mark.parametrize('test_input,expected', test_data)
+    def test_parse_file(self, test_input, expected, additional_parser_params):
+        urls, words_to_find = test_input
+        assert WebParser(urls, words_to_find, **additional_parser_params).parse().count_words_occurrence() == expected
+
+    @pytest.mark.parametrize(
+        'test_input,expected', [
+            ((['python'], 'python Pythonas mvfkvm fjf python'), ['python', 'Python', 'python']),
+            ((['flask'], 'flaskFLASKfLask flask FLAK'), ['flask', 'FLASK', 'fLask', 'flask']),
+            ((['linux'], 'linux, Linux, linux lin'), ['linux', 'Linux', 'linux'])
+        ])
+    def test__findall_in_page(self, test_input, expected):
+        key, text = test_input
+        assert WebParser([''], key)._findall_in_page(text) == expected
