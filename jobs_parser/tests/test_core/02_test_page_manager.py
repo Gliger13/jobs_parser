6870a37377ce0218e27054955ccc06ca8c49923d
Gliger13 <zanevskiyandrey@gmail.com>
2021-02-13 23:32:01 +0300

    Tests: add tests for core level
    Also create test configuration file with priority marks

--- jobs_parser/tests/test_core/02_test_page_manager.py
+++ jobs_parser/tests/test_core/02_test_page_manager.py
@@ -1,29 +1,49 @@
+import pytest
+
 from core.page_manager import Page, Pages
 
 
+@pytest.fixture
+def request_headers():
+    return {'user-agent': 'job_parser/0.1.0'}
+
+
+@pytest.fixture(params=[
+    'https://rabota.by/', 'https://google.com/', 'https://yandex.ru/'
+])
+def valid_url(request):
+    return request.param
+
+
+@pytest.fixture(params=[
+    'https://rabota.by/404', 'https://google.com/404', 'https://yandex.ru/404'
+])
+def url_with_404(request, request_headers):
+    return request.param
+
+
+@pytest.fixture
+def valid_urls():
+    return ['https://rabota.by/', 'https://google.com/', 'https://yandex.ru/']
+
+
+@pytest.mark.smoke
 class TestPage:
-    def setup(self):
-        self.request_headers = {'user-agent': 'job_parser/0.1.0'}
+    def test_is_page_exist(self, valid_url, request_headers):
+        assert Page(valid_url, request_headers).is_page_exist()
 
-    def test_is_page_exist(self):
-        assert Page('https://rabota.by/', self.request_headers).is_page_exist()
-
-    def test_is_not_page_exist(self):
-        assert not Page('https://google.com/404', self.request_headers).is_page_exist()
+    def test_is_not_page_exist(self, url_with_404, request_headers):
+        assert not Page(url_with_404, request_headers).is_page_exist()
 
 
+@pytest.mark.advance
 class TestPages:
-    def setup(self):
-        self.request_headers = {'user-agent': 'job_parser/0.1.0'}
-        self.urls = ['https://rabota.by/', 'https://google.com/']
-        self.url_with_404 = 'https://google.com/404'
+    def test_is_pages_exist(self, valid_urls, request_headers):
+        assert Pages(valid_urls, request_headers).is_pages_exist()
 
-    def test_is_pages_exist(self):
-        assert Pages(self.urls, self.request_headers).is_pages_exist()
+    def test_is_not_pages_exist(self, valid_urls, url_with_404, request_headers):
+        valid_urls.append(url_with_404)
+        assert not Pages(valid_urls, request_headers).is_pages_exist()
 
-    def test_is_not_pages_exist(self):
-        self.urls.append(self.url_with_404)
-        assert not Pages(self.urls, self.request_headers).is_pages_exist()
-
-    def test_get_files(self):
-        assert len(Pages(self.urls, self.request_headers).get_files()) == 2
+    def test_get_files(self, valid_urls, request_headers):
+        assert len(Pages(valid_urls, request_headers).get_files()) == 3
