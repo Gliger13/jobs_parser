8634602dbd8c31791a6a10092bdd5c538e758590
Gliger13 <zanevskiyandrey@gmail.com>
2021-02-14 03:01:44 +0300

    Test: add tests for parse results

---
+++ jobs_parser/tests/test_business/test_parse_results.py
@@ -0,0 +1,16 @@
+import pytest
+
+from business.parse_results import ParseResult
+
+
+class TestParseResult:
+    @pytest.mark.parametrize('test_input,expected', [
+        (['Linux', 'linux', 'flask', 'Linux', 'Flask', 'Linux', 'Flask', 'linux', 'Flask'],
+         {'python': 0, 'linux': 5, 'flask': 4}),
+        (['Linux', 'Linux', 'Linux', 'Linux', 'Linux'],
+         {'python': 0, 'linux': 5, 'flask': 0}),
+        (['Python', 'Linux', 'Flask', 'Python', 'Linux', 'Python'],
+         {'python': 3, 'linux': 2, 'flask': 1})
+    ])
+    def test_count_word(self, test_input, expected):
+        return ParseResult('', ['python', 'linux', 'Flask'], test_input) == expected
